457c89d752c56d4f255a3c54ab227e0316494c03
Rafoolin <niloo.jv@gmail.com>
2022-08-02 04:41:14 +0200

    feat(server): update "tr_fn_tasks_bu" on category changes

--- server/nhost/migrations/default/1659038580667_add_tr_fn_tasks_bu/up.sql
+++ server/nhost/migrations/default/1659038580667_add_tr_fn_tasks_bu/up.sql
@@ -5,12 +5,25 @@ CREATE OR REPLACE FUNCTION public.tr_fn_tasks_bu ()
     AS $tr_fn_tasks_bu$
 BEGIN
     IF TG_OP = 'UPDATE' THEN
-        NEW.completed_at := CASE NEW.completed
-        WHEN TRUE THEN
-            now()
-        ELSE
-            NULL
-        END;
+        -- Update the [completed_at] based on [completed]
+        IF (NEW.completed != OLD.completed) THEN
+            NEW.completed_at := CASE NEW.completed
+            WHEN TRUE THEN
+                now()
+            ELSE
+                NULL
+            END;
+        END IF;
+        -- If user change the [category_id], update the [category_color] based on that
+        IF (NEW.category_id != OLD.category_id) THEN
+            NEW.category_color :=
+            (SELECT
+                color
+            FROM
+                categories
+            WHERE
+                id = NEW.category_id);
+        END IF;
     END IF;
     RETURN NEW;
 END;
@@ -18,9 +31,9 @@ $tr_fn_tasks_bu$
 LANGUAGE plpgsql;
 
 DROP TRIGGER IF EXISTS tr_tasks_bu ON "public"."tasks";
+
 CREATE TRIGGER tr_tasks_bu
-    BEFORE UPDATE OF completed ON "public"."tasks"
+    BEFORE UPDATE ON "public"."tasks"
     FOR EACH ROW
-    WHEN (OLD.completed IS DISTINCT FROM NEW.completed)
     EXECUTE FUNCTION tr_fn_tasks_bu ();
 
